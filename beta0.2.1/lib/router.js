cb13af415232af4f7a9539ad6783f7f02353dfb8
Steven Noonan <stevennoonan013@gmail.com>
2017-03-27 14:25:04 +0100

    Working client version

---
+++ beta0.2.1/lib/router.js
@@ -0,0 +1,30 @@
+//router
+//Router.route('/', function (){
+//    this.render('Home', {
+//        data: function() {return Items.findOne({id: this.params._id});}
+//    })
+//});
+
+//home page
+Router.route('/', function(){
+    this.layout('navbar')
+    this.render('Home');
+});
+
+//Ad page
+Router.route('adpage', function(){
+    this.layout('navbar')
+    this.render('adPage');
+})
+
+//All ads
+Router.route('allads', function(){
+    this.layout('navbar')
+    this.render('allAds');
+})
+
+//Add new advertisment form
+Router.route('addnewad', function(){
+    this.layout('navbar')
+    this.render('form');
+})
